3cfe5642bc037797fe3c0afb87170514b4858c68
unknown <herlambang923@gmail.com>
2023-02-18 18:08:56 +0700

    latihan pertemuan 2

---
+++ kuliah/pertemuan2/index.php
@@ -0,0 +1,27 @@
+<?php
+$nama = "Dimas Nanda Herlambang";
+$hari = 'Hari ini kita "ujian"';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pertemuan 2</title>
+</head>
+
+<body>
+
+  <h1>
+
+    <?php echo "Hello, $nama!";  ?>
+
+  </h1>
+  <P><?php echo "$hari Pemograman Web"; ?></P>
+
+</body>
+
+</html>
