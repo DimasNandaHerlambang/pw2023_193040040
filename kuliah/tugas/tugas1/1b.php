5905aa8604f24c44c1d60f07cb7119b53c786336
unknown <herlambang923@gmail.com>
2023-02-20 21:00:30 +0700

    1b

---
+++ kuliah/tugas/tugas1/1b.php
@@ -0,0 +1,34 @@
+<?php
+$nrp = 40;
+$kali = 5;
+$bagi = 2;
+$tambah = 75;
+$kurang = 20;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>tugas 1b</title>
+</head>
+
+<body>
+
+  <?php
+  echo "Aku adalah angka $nrp";
+  echo "</br>";
+  echo $nrp * $kali;
+  echo "</br>";
+  echo $nrp / $bagi;
+  echo "</br>";
+  echo $nrp + $tambah;
+  echo "</br>";
+  echo $nrp - $kurang;
+  ?>
+
+</body>
+
+</html>
