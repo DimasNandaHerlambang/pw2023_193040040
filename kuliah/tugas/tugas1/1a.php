e3de3e98b64fadc6a1920207bdbc0bb3006e0517
unknown <herlambang923@gmail.com>
2023-02-20 19:35:52 +0700

    tugas 1

---
+++ kuliah/tugas/tugas1/1a.php
@@ -0,0 +1,30 @@
+<?php
+$topi = "Topi saya Bundar";
+$bundar = "Bundar Topi saya.";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tugas 1a</title>
+</head>
+
+<body>
+
+  <h2>
+
+    <i>
+      <?php echo "$topi, $bundar"; ?>
+    </i>
+
+  </h2>
+
+</body>
+
+</html>
